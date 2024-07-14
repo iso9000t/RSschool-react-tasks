f0496ac92b8d26639d42f95f5ab81d50f23de23c
iso9000t <sasha.gr81@gmail.com>
2024-07-14 21:25:48 +0400

    feat: add react router

--- project-one/src/main.tsx
+++ project-one/src/main.tsx
@@ -2,9 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom';
+import NotFound from './components/NotFound/NotFound.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Navigate to="/main" />
+  },
+  {
+    path: '/main',
+    element: <App />,
+    errorElement: <NotFound/>
+  },
+  {
+    path: '*',
+    element: <NotFound/>
+  }
+]);
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
